48da6a4067e583e60b99d52379f5c5f7a6aa41cf
Mark Burgess <mburgess1983@gmail.com>
2025-08-18 23:29:06 +0100

    initial

--- newworld4/src/App.tsx
+++ newworld4/src/App.tsx
@@ -1,37 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from "react-router-dom";  // Link
+import { loginRequest } from './authConfig';
 import './App.css'
+import { useMsal } from '@azure/msal-react';
+import Dashboard from "./screens/dashboard/Dashboard";
+import Home from "./screens/home/Home";
+import Plain from "./screens/plain/Plain";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
+    const { instance, accounts } = useMsal();
+
+    const handleLogin = () => {
+        instance.loginPopup(loginRequest).catch(e => {
+            console.error(e);
+        });
+    };
+
+    const handleLogout = () => {
+        instance.logoutPopup();
+    };
 
   return (
-    <>
-          <div>
+      <>
 
-              <button className="btn btn-primary">Default</button>
-              <button className="btn btn-secondary">Default</button>
+          <BrowserRouter>
+              <Routes>
+                  <Route path="/dashboard" element={<Dashboard />}></Route>
+                  <Route path="/home" element={<Home />}></Route>
+                  <Route path="/" element={<Plain />}></Route>
+              </Routes>
+          </BrowserRouter>
 
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="bg-red-400">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+          {accounts.length > 0 ? (
+              <>
+                  <p>Welcome, {accounts[0].username}</p>
+                  <button onClick={handleLogout}>Logout</button>
+              </>
+          ) : (
+              <button onClick={handleLogin}>Login with Azure</button>
+          )
+          }
     </>
   )
 }
