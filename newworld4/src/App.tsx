a89ce575c63b4185d47fe3aafb465a23506c53ba
Mark Burgess <mburgess1983@gmail.com>
2025-08-18 23:14:07 +0100

    initial

--- newworld4/src/App.tsx
+++ newworld4/src/App.tsx
@@ -8,7 +8,11 @@ function App() {
 
   return (
     <>
-      <div>
+          <div>
+
+              <button className="btn btn-primary">Default</button>
+              <button className="btn btn-secondary">Default</button>
+
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
