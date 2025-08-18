a7d692964d7e508f8bc9062996f20242d8539542
Mark Burgess <mburgess1983@gmail.com>
2025-08-18 23:40:27 +0100

    initial

--- newworld4/src/main.tsx
+++ newworld4/src/main.tsx
@@ -1,18 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App'
-import { MsalProvider } from '@azure/msal-react';
-import { msalConfig } from './authConfig';
-import { PublicClientApplication } from '@azure/msal-browser';
-import React from 'react';
-
-const msalInstance = new PublicClientApplication(msalConfig);
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <MsalProvider instance={msalInstance}>
-            <App />
-        </MsalProvider>
+        <App />
     </StrictMode>,
 )
