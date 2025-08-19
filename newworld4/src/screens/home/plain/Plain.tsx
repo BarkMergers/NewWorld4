d68ded8b2f328340da0a552f707395f7d5868a70
Mark Burgess <mburgess1983@gmail.com>
2025-08-19 15:18:22 +0100

    fix bug

--- newworld4/src/screens/home/plain/Plain.tsx
+++ newworld4/src/screens/home/plain/Plain.tsx
@@ -1,9 +1,7 @@
-import type { AccountInfo } from "@azure/msal-browser";
-
 import StatsBar from "../../../elements/statsBar/StatsBar";
 import TitleBar from "../../../elements/titleBar/TitleBar";
 
-export default function Plain({ accounts }: { accounts: AccountInfo[] } ) {
+export default function Plain() {
     return (
         <>
             <TitleBar title="NewWORLD"></TitleBar>
