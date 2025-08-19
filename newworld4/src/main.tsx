99b3b7e0436330e0e458a7ace7982d8e2dca5922
Mark Burgess <mburgess1983@gmail.com>
2025-08-19 07:14:56 +0100

    add login

--- newworld4/src/main.tsx
+++ newworld4/src/main.tsx
@@ -3,13 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { PublicClientApplication } from '@azure/msal-browser';
+import { MsalProvider } from '@azure/msal-react';
+
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 const queryClient = new QueryClient;
 
+
+const msalInstance = new PublicClientApplication(msalConfig);
+
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <QueryClientProvider client={queryClient}>
-            <App />
-        </QueryClientProvider>
+        <MsalProvider instance={msalInstance}>
+            <QueryClientProvider client={queryClient}>
+                <App />
+            </QueryClientProvider>
+        </MsalProvider>  
     </StrictMode>,
 )
